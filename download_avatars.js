028277f656ca12ed01cc5357ce1c7dcc8c807ba6
Bosco <bleung9@gmail.com>
2019-03-12 00:43:37 +0000

    finished avatar downloader w/o the strecth

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,14 @@
 // c1c6a20f0c56c39b79173ae172089dad1e90a4c9
 
+//require the requisite libraries
 var request = require("request");
 var secrets = require("./secrets.js");
 var fs = require('fs');
 
+//invoked from main fxn w/ an anon callback
 function getRepoContributors(repoOwner, repoName, cb) {
+
+  //options is...a thing for the request library to parse through?  if i recall correctly?
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
@@ -12,12 +16,20 @@ function getRepoContributors(repoOwner, repoName, cb) {
       "Authorization": secrets.GITHUB_TOKEN
     }
   }
+
+  //parse the JSON string into an object, and pass the object into
+  //the callback function (retrieves URL from array of objects for each user)
+  //and produces a filePath to send that picture to on my computer
   request(options, function(err, res, body) {
     let obj = JSON.parse(body);
     cb(err, obj);
   });
 };
 
+
+
+
+//download images based on URL and sends them to a filepath on my computer
 function downloadImageByURL(url, filePath) {
   request.get(url)
        .on("error", function(err) {
@@ -32,17 +44,25 @@ function downloadImageByURL(url, filePath) {
        })
 }
 
+
+
 console.log("HELLO FELLOW DENIZEN OF THE INTERNET! WELCOME TO THE GITHUB AVATAR DOWNLOADER!");
 
+//input reponame and repo owner to retrieve pictures from
 let input = process.argv;
 
+//check for two names (strings)
 if (input.length === 3) {
   console.log("YOU'RE AN IDIOT, YOU'RE MISSING A REPO NAME/OWNER!!!!!!!!!!!!!!!!!!");
 } else if (input.length === 2) {
   console.log("YOU'RE A MASSIVE IDIOT, YOU'RE MISSING BOTH THE REPO NAME AND OWNER!!!!!!!!!!!!!!!!!!");
 } else {
+  //take input reponame and username
   let owner = process.argv[2];
   let name = process.argv[3];
+
+  //get repo contributors, pass an anon callback that will download images into getRepoContributors.
+  //the callback is invoked by request on line 25 "cb(err,obj)"
   getRepoContributors(owner, name, function(err, result) {
     // console.log("Errors:", err);
     for (element of result) {
